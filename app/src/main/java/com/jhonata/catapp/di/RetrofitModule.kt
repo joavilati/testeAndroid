102d9f19a04670d16b848cc53e330e41cc601b3f
Jhonata Ávila <jo.avilati@gmail.com>
2021-04-11 21:57:46 -0300

    refac(di): adjust injection.

--- app/src/main/java/com/jhonata/catapp/di/RetrofitModule.kt
+++ app/src/main/java/com/jhonata/catapp/di/RetrofitModule.kt
@@ -1,12 +1,12 @@
 package com.jhonata.catapp.di
 
 import com.jhonata.catapp.retrofit.TheCatApi
-import com.jhonata.catapp.util.SupportInterceptor
-import com.jhonata.catapp.util.baseUrl
+import com.jhonata.catapp.util.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,12 +15,12 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object RetrofitModule {
 
     @Singleton
     @Provides
-    fun provideTheCatService(): TheCatApi {
+    fun provideTheCatService(supportInterceptor: Interceptor): TheCatApi {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.HEADERS
         interceptor.level = HttpLoggingInterceptor.Level.BODY
@@ -28,15 +28,28 @@ object RetrofitModule {
             addInterceptor(interceptor)
             connectTimeout(20, TimeUnit.SECONDS)
             readTimeout(20, TimeUnit.SECONDS)
-            addInterceptor(SupportInterceptor())
+            addInterceptor(supportInterceptor)
         }
 
         val retrofit = Retrofit.Builder().run {
             client(builder.build())
-            baseUrl(baseUrl)
+            baseUrl(BASE_URL)
             addConverterFactory(GsonConverterFactory.create())
             build()
         }
         return retrofit.create(TheCatApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providesSupportInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val request = chain.request().newBuilder().run {
+                addHeader("Content-Type", "application/json")
+                addHeader("Accept", "application/json")
+                build()
+            }
+            chain.proceed(request)
+        }
     }
 }
--- app/src/main/java/com/jhonata/catapp/util/SupportInterceptor.kt
+++ app/src/main/java/com/jhonata/catapp/util/SupportInterceptor.kt
@@ -1,17 +1,3 @@
 package com.jhonata.catapp.util
 
-import okhttp3.Interceptor
-import okhttp3.Response
-
-const val baseUrl = "https://www.dnd5eapi.co/api/"
-
-class SupportInterceptor : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request().newBuilder().run {
-            addHeader("Content-Type", "application/json")
-            addHeader("Accept","application/json")
-            build()
-        }
-        return  chain.proceed(request)
-    }
-}
+const val BASE_URL = "https://www.dnd5eapi.co/api/"
